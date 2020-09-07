e0e22697ca77929bde8db275c2da3d0d0e1b6e79
Rodrigo Bonifacio <rbonifacio123@gmail.com>
2020-09-07 08:34:46 -0300

    compute basic expression types

--- src/main/scala/br/unb/cic/oberon/tc/TypeChecker.scala
+++ src/main/scala/br/unb/cic/oberon/tc/TypeChecker.scala
@@ -1,18 +1,34 @@
 package br.unb.cic.oberon.tc
 
-import br.unb.cic.oberon.ast.{BoolValue, BooleanType, Constant, Expression, FormalArg, IntValue, IntegerType, OberonModule, Procedure, Statement, Type, Undef, VariableDeclaration}
+import br.unb.cic.oberon.ast.{AddExpression, BoolValue, BooleanType, Constant, DivExpression, EQExpression, Expression, FormalArg, GTEExpression, GTExpression, IntValue, IntegerType, LTEExpression, LTExpression, MultExpression, NEQExpression, OberonModule, Procedure, Statement, SubExpression, Type, Undef, VariableDeclaration}
 import br.unb.cic.oberon.visitor.{OberonVisitor, OberonVisitorAdapter}
 
-//class ComputeExpressionType extends OberonVisitorAdapter {
-//  type T = Option[Type]
-//
-//  override def visit(exp: Expression): Unit = exp match {
-//    case IntValue(v) => result = Some(IntegerType)
-//    case BoolValue(v) => result = Some(BooleanType)
-//    case Undef() => result = None
-//    case
-//  }
-//}
+class ExpressionTypeVisitor extends OberonVisitorAdapter {
+  type T = Option[Type]
+
+  override def visit(exp: Expression): Option[Type] = exp match {
+    case IntValue(_) => Some(IntegerType)
+    case BoolValue(_) => Some(BooleanType)
+    case Undef() => None
+    case EQExpression(left, right) => computeBinExpressionType(left, right, IntegerType, BooleanType)
+    case NEQExpression(left, right) => computeBinExpressionType(left, right, IntegerType, BooleanType)
+    case GTExpression(left, right) => computeBinExpressionType(left, right, IntegerType, BooleanType)
+    case LTExpression(left, right) => computeBinExpressionType(left, right, IntegerType, BooleanType)
+    case GTEExpression(left, right) => computeBinExpressionType(left, right, IntegerType, BooleanType)
+    case LTEExpression(left, right) => computeBinExpressionType(left, right, IntegerType, BooleanType)
+    case AddExpression(left, right) => computeBinExpressionType(left, right, IntegerType, IntegerType)
+    case SubExpression(left, right) => computeBinExpressionType(left, right, IntegerType, IntegerType)
+    case MultExpression(left, right) => computeBinExpressionType(left, right, IntegerType, IntegerType)
+    case DivExpression(left, right) => computeBinExpressionType(left, right, IntegerType, IntegerType)
+    // TODO: function call ...
+  }
+
+  def computeBinExpressionType(left: Expression, right: Expression, expected: Type, result: Type) : Option[Type] = {
+    val t1 = left.accept(this)
+    val t2 = right.accept(this)
+    if(t1 == t2 && t1 == Some(expected)) Some(result) else None
+  }
+}
 //
 //class TypeChecker extends OberonVisitorAdapter {
 //  type T = List[String]
