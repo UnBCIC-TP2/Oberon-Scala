b72061d64e4980e17bdf67e9e1ca43189e5414b8
eduardocserra <eduardocserra@outlook.com>
2020-09-14 21:29:43 -0300

    Adicionada leitura de cabeçalho para c

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,12 @@
+import br.unb.cic.oberon.ast._
+
 object Main extends App {
-  println("Hello, World!")
+  val arg1 = FormalArg("x", IntegerType);
+  val arg2 = FormalArg("y", BooleanType);
+  val p1 = Procedure("pro1", List(arg1, arg2), Some(IntegerType), List[Constant](), List[VariableDeclaration](), null);
+  val p2 = Procedure("pro2", List(arg2, arg1), Some(BooleanType), List[Constant](), List[VariableDeclaration](), null);
+  val p3 = Procedure("pro3", List(arg2, arg1), None, List[Constant](), List[VariableDeclaration](), null);
+  val module = OberonModule("tESTAObeRon", List[Constant](), List[VariableDeclaration](), List(p1, p2, p3), None);
+  val codeGen = CCodeGenerator()
+  codeGen.readProcedure(module);
 }
--- src/main/scala/br/unb/cic/oberon/ast/CodeGenerator.scala
+++ src/main/scala/br/unb/cic/oberon/ast/CodeGenerator.scala
@@ -1,14 +1,36 @@
-package ast
+package br.unb.cic.oberon.ast
 
 
 trait CodeGenerator {
-    def read_procedure(module: OberonModule)
+    def readProcedure(module: OberonModule)
 }
 
 
 case class CCodeGenerator () extends CodeGenerator {
 
-    def read_procedure(module:OberonModule): T {
-        module
+    def readProcedure(module: OberonModule) = {
+        for (procedure <- module.procedures) {
+            var args = "";
+            var returnType = "";
+            procedure.returnType match {
+                case Some(IntegerType) => returnType = "int"
+                case Some(BooleanType) => returnType = "bool"
+                case None => returnType = "void"
+                case _ => returnType = "undefined"
+            }
+            for((arg, index) <- procedure.args.view.zipWithIndex) {
+                var argumentType = "";
+                arg.argumentType match {
+                    case IntegerType => argumentType = "int"
+                    case BooleanType => argumentType = "bool"
+                    case _ => argumentType = "undefined"
+                }
+                args = args + s"${argumentType} ${arg.name}"
+                if (index + 1 < procedure.args.length)
+                    args = args + ", "
+            }
+            println(s"${returnType} ${procedure.name} (${args})")
+        }
     }
 }
+
