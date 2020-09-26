7313ffa07e9d9fd68ac4890fe32f8858b31491d3
Raylan Sales <raylanwork@gmail.com>
2020-09-26 20:11:09 -0300

    Updated interpreter_stmt04.oberon

--- src/test/scala/br/unb/cic/oberon/interpreter/InterpreterTest.scala
+++ src/test/scala/br/unb/cic/oberon/interpreter/InterpreterTest.scala
@@ -144,21 +144,4 @@ test("Testing interpreter on interpreter_stmt04 program") {
 
   }
 
-// test("Testing interpreter on interpreter_stmt05 program") {
-//     val path = Paths.get(getClass.getClassLoader.getResource("stmts/interpreter_stmt05.oberon").getFile)
-
-//     assert(path != null)
-
-//     val content = String.join("\n", Files.readAllLines(path))
-//     val module = ScalaParser.parse(content)
-//     val interpreter = new Interpreter()
-//     assert(module.name == "SimpleModule")
-
-//     module.accept(interpreter)
-	
-// 	assert(interpreter.env.lookup("x") == Some(IntValue(90)))
-//   assert(interpreter.env.lookup("z") == Some(IntValue(50))) 
-
-//   }
-
 }
