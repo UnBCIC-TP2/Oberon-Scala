6a950e47f29c4da76b3ba1a453ef6c51f7a06f6c
Eduardo Castro Serra <eduardocserra@outlook.com>
2020-09-28 10:19:39 -0300

    Adicionados stmts de readm write, procedure, ifelse e while no codegenerator

--- src/main/scala/br/unb/cic/oberon/codegen/CCodeGenerator.scala
+++ src/main/scala/br/unb/cic/oberon/codegen/CCodeGenerator.scala
@@ -3,10 +3,7 @@ import br.unb.cic.oberon.ast._
 import scala.annotation.tailrec
 import org.typelevel.paiges._
 
-abstract class CCodeGenerator extends CodeGenerator {
-
-}
-
+abstract class CCodeGenerator extends CodeGenerator {}
 
 case class PaigesBasedGenerator() extends CCodeGenerator {
   override def generateCode(module: OberonModule): String = {
@@ -19,30 +16,39 @@ case class PaigesBasedGenerator() extends CCodeGenerator {
   def generateProcedure(procedure: Procedure): Doc = {
     var returnType = Doc.empty;
     procedure.returnType match {
-        case Some(IntegerType) => returnType = Doc.text("int")
-        case Some(BooleanType) => returnType = Doc.text("bool")
-        case None => returnType = Doc.text("void")
-        case _ => returnType = Doc.text("undefined")
+      case Some(IntegerType) => returnType = Doc.text("int")
+      case Some(BooleanType) => returnType = Doc.text("bool")
+      case None              => returnType = Doc.text("void")
+      case _                 => returnType = Doc.text("undefined")
     }
-    val args = procedure.args.map { case (arg) =>
-      var argumentType = Doc.empty;
-      arg.argumentType match {
+    val args = procedure.args.map {
+      case (arg) =>
+        var argumentType = Doc.empty;
+        arg.argumentType match {
           case IntegerType => argumentType = Doc.text("int")
           case BooleanType => argumentType = Doc.text("bool")
-          case _ => argumentType = Doc.text("undefined")
-      }
-      argumentType + Doc.space + Doc.text(arg.name)
+          case _           => argumentType = Doc.text("undefined")
+        }
+        argumentType + Doc.space + Doc.text(arg.name)
     }
     val procedureArgs = Doc.intercalate(Doc.char(',') + Doc.space, args)
-    val procedureName = returnType + Doc.space + Doc.text(procedure.name) + Doc.char('(')
-    val procedureHeader = procedureArgs.tightBracketBy(procedureName, Doc.char(')'))
+    val procedureName =
+      returnType + Doc.space + Doc.text(procedure.name) + Doc.char('(')
+    val procedureHeader =
+      procedureArgs.tightBracketBy(procedureName, Doc.char(')'))
     val procedureBody = generateStatement(procedure.stmt)
-    procedureHeader + Doc.space + Doc.char('{') + Doc.line + procedureBody + Doc.char('}')
+    procedureHeader + Doc.space + Doc.char('{') + Doc.line + procedureBody + Doc
+      .char('}')
   }
 
   def generateStatement(statement: Statement): Doc = {
     statement match {
-      case AssignmentStmt(varName, expression) => Doc.space + Doc.text(varName) + Doc.space + Doc.char('=') + Doc.space + generateExpression(expression) + Doc.char(';') + Doc.line
+      case AssignmentStmt(varName, expression) =>
+        Doc.space + Doc.text(varName) + Doc.space + Doc.char(
+          '='
+        ) + Doc.space + generateExpression(expression) + Doc.char(
+          ';'
+        ) + Doc.line
       case SequenceStmt(stmts) => {
         var multipleStmts = Doc.empty
         for (stmt <- stmts) {
@@ -50,32 +56,96 @@ case class PaigesBasedGenerator() extends CCodeGenerator {
         }
         multipleStmts
       }
+      case ReadIntStmt(varName) =>
+        Doc.space + Doc.text("scanf(\"%d\", &") + Doc.text(varName) + Doc.text(
+          ");"
+        ) + Doc.line
+      case WriteStmt(expression) =>
+        Doc.space + Doc.text("printf(\"%d\", ") + generateExpression(
+          expression
+        ) + Doc.text(");") + Doc.line
+      case ProcedureCallStmt(name, args) => {
+        val expressions = args.map {
+          case (arg) =>
+            generateExpression(arg)
+        }
+        val functionArgs =
+          Doc.intercalate(Doc.char(',') + Doc.space, expressions)
+        functionArgs.tightBracketBy(
+          Doc.text(name) + Doc.char('('),
+          Doc.char(')')
+        )
+      }
+      case IfElseStmt(condition, thenStmt, elseStmt) => {
+        val ifCond = Doc.space + Doc.text("if (") + generateExpression(
+          condition
+        ) + Doc.text(
+          ")"
+        ) + Doc.line + generateStatement(thenStmt).tightBracketBy(
+          Doc.text(" {"),
+          Doc.text(" }")
+        )
+        val elseCond = elseStmt match {
+          case Some(stmt) => {
+            Doc.space + Doc.text("else ") + generateStatement(
+              thenStmt
+            ).tightBracketBy(
+              Doc.text(" {"),
+              Doc.text(" }")
+            )
+          }
+          case None => Doc.empty
+        }
+        ifCond + elseCond
+      }
+      case WhileStmt(condition, stmt) =>
+        Doc.space + Doc.text("while (") + generateExpression(
+          condition
+        ) + Doc.text(
+          ")"
+        ) + Doc.line + generateStatement(stmt).tightBracketBy(
+          Doc.text(" {"),
+          Doc.text(" }")
+        )
       case _ => Doc.empty
     }
-    
+
   }
 
   def generateExpression(expression: Expression): Doc = {
     expression match {
-      case IntValue(v) => Doc.text(v.toString())
-      case BoolValue(v) => Doc.text(if (v) "true" else "false")
-      case Undef() => Doc.text("undefined")
+      case IntValue(v)         => Doc.text(v.toString())
+      case BoolValue(v)        => Doc.text(if (v) "true" else "false")
+      case Undef()             => Doc.text("undefined")
       case VarExpression(name) => Doc.text(name)
-      case Brackets(exp) => Doc.char('(') + Doc.space + generateExpression(exp) + Doc.space + Doc.char(')')
+      case Brackets(exp) =>
+        Doc.char('(') + Doc.space + generateExpression(exp) + Doc.space + Doc
+          .char(')')
       case EQExpression(left, right) => generateBinExpression(left, right, "==")
       case FunctionCallExpression(name, args) => {
-        val expressions = args.map { case (arg) =>
-          generateExpression(arg)
+        val expressions = args.map {
+          case (arg) =>
+            generateExpression(arg)
         }
-        val functionArgs = Doc.intercalate(Doc.char(',') + Doc.space, expressions)
-        functionArgs.tightBracketBy(Doc.text(name) + Doc.char('('), Doc.char(')'))
+        val functionArgs =
+          Doc.intercalate(Doc.char(',') + Doc.space, expressions)
+        functionArgs.tightBracketBy(
+          Doc.text(name) + Doc.char('('),
+          Doc.char(')')
+        )
       }
       case _ => Doc.empty
     }
-    
+
   }
-  def generateBinExpression(left: Expression, right: Expression, sign: String): Doc = 
-    generateExpression(left) + Doc.space + Doc.text(sign) + Doc.space + generateExpression(right)
+  def generateBinExpression(
+      left: Expression,
+      right: Expression,
+      sign: String
+  ): Doc =
+    generateExpression(left) + Doc.space + Doc.text(
+      sign
+    ) + Doc.space + generateExpression(right)
 }
 
 class PPrintBasedGenerator extends CCodeGenerator {
