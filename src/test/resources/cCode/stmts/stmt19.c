9827d8d51e7f0bed68417d85c4455e1040261db7
Mateus <mateusoliveirap@hotmail.com>
2020-11-01 04:41:07 -0300

    Criado o stmt19.c

---
+++ src/test/resources/cCode/stmts/stmt19.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int n1,n2,n3;
+    
+    scanf("%d",&n1);
+    scanf("%d",&n2);    
+    scanf("%d",&n3);
+    if((n1>n2)&&(n1>n3))
+        printf("%d", n1);
+    else if((n2>n3)&&(n2>n1))
+        printf("%d", n2);
+    else
+        printf("%d", n3);
+}
