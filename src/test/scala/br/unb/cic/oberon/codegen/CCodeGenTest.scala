3b556f0936c19dd2b73e3a78ffd1a513caecbdf8
Mateus <mateusoliveirap@hotmail.com>
2020-11-01 04:50:20 -0300

    Criado os testes para stmt32.oberon e stmt19.c

--- src/test/scala/br/unb/cic/oberon/codegen/CCodeGenTest.scala
+++ src/test/scala/br/unb/cic/oberon/codegen/CCodeGenTest.scala
@@ -235,4 +235,29 @@ class CCodeGenTest extends AnyFunSuite {
     val cCode = String.join("\n", Files.readAllLines(cPath))
     assert(generatedCCode == cCode)
   }
+
+  test("Testing C generator for stmt32") {
+    val oberonPath = Paths.get(
+      getClass.getClassLoader
+        .getResource("stmts/stmt32.oberon")
+        .getFile
+        .replace("/C:/", "C:/")
+    )
+    assert(oberonPath != null)
+
+    val oberonContent = String.join("\n", Files.readAllLines(oberonPath))
+    val module = ScalaParser.parse(oberonContent)
+    val codeGen = PaigesBasedGenerator()
+    val generatedCCode = codeGen.generateCode(module)
+
+    val cPath = Paths.get(
+      getClass.getClassLoader
+        .getResource("cCode/stmts/stmt19.c")
+        .getFile
+        .replace("/C:/", "C:/")
+    )
+    assert(cPath != null)
+    val cCode = String.join("\n", Files.readAllLines(cPath))
+    assert(generatedCCode == cCode)
+  }
 }
