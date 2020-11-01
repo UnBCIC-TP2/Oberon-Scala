bd631ec2b820d3f8bf042c17cc4d1f0266aea0ca
kdusousa <kdusousa@gmail.com>
2020-11-01 11:53:49 -0300

    CCodeGenerator for IfElseIfStmt

--- src/test/resources/cCode/stmts/stmt17.c
+++ src/test/resources/cCode/stmts/stmt17.c
@@ -3,18 +3,19 @@
 
 void main() {
   int x, y;
+
   scanf("%d", &x);
-  if (x < 5){
+  if (x < 5) {
     y = 1;
   }
-  else if (x < 7){
+  else if (x < 7) {
     y = 2;
   }
-  else if (x < 9){
-    y=3;
+  else if (x < 9) {
+    y = 3;
   }
-  else
+  else {
     y = 4;
-
+  }
   printf("%d\n", y);
 }
