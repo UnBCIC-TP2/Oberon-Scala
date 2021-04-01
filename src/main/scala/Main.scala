5a995aa0e1b78c899578eea8d82494d7e5a357d0
trajano7 <matheus.trajano48@hotmail.com>
2021-03-31 22:14:31 -0300

    Introdução da primeira versão do command line de TypeChecker, precisa ser refinada.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,33 @@
-object Main extends App {
-  println("Hello, World!")
+import br.unb.cic.oberon.parser.ScalaParser
+import br.unb.cic.oberon.tc.TypeChecker
+import org.rogach.scallop._
+
+import java.nio.file.{Files, Paths}
+
+class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val tyc = opt[String]()
+  verify()
+}
+
+object Main {
+  def main(args: Array[String]) {
+    val conf = new Conf(args)
+    if(conf.tyc.isSupplied) {
+      //println("Escalobaloba " + conf.tyc())
+      val path = Paths.get(conf.tyc())
+      //println("chegou aqui")
+      /*if(path == null) println("null")*/
+      //println(path)
+      val content = String.join("\n", Files.readAllLines(path))
+      //println(content)
+
+      val module = ScalaParser.parse(content)
+      val visitor = new TypeChecker()
+
+      val errors = visitor.visit(module)
+      if(errors.size == 0) println("The code is correctly typed")
+      else println("Type error detected")
+    }
+
+  }
 }
