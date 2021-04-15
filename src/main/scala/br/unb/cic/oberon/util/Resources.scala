db9e3c2030c3b5772999c538e15839a0497dd980
LeoRiether <leonardoalves8217@gmail.com>
2021-04-15 09:12:18 -0300

    Try to fix Resources.getContent

--- src/main/scala/br/unb/cic/oberon/util/Resources.scala
+++ src/main/scala/br/unb/cic/oberon/util/Resources.scala
@@ -1,9 +1,12 @@
 package br.unb.cic.oberon.util
 
+import java.nio.file.{Paths, Files}
+
 object Resources {
   def getContent(resource: String): String = {
-    val stream = getClass.getClassLoader.getResourceAsStream(resource)
-    val content = new String(stream.readAllBytes, "utf-8").replaceAll("\r\n", "\n")
+    val path = Paths.get(getClass.getClassLoader.getResource(resource).toURI)
+    assert(path != null)
+    val content = String.join("\n", Files.readAllLines(path))
     content
   }
 }
