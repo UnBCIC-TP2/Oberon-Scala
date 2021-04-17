e7fa81bd2575ef8a2897760e3ab69bd2296df9a6
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-04-17 18:38:57 -0300

    Fix resources reading using scala libs
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/main/scala/br/unb/cic/oberon/util/Resources.scala
+++ src/main/scala/br/unb/cic/oberon/util/Resources.scala
@@ -1,13 +1,18 @@
 package br.unb.cic.oberon.util
 
 import scala.io.Source
-import java.nio.file.{Paths, Files}
+import java.nio.file.Paths
 
 object Resources {
   def getContent(resource: String): String = {
     val path = Paths.get(getClass.getClassLoader.getResource(resource).toURI)
     assert(path != null)
-    val content = String.join("\n", Files.readAllLines(path))
+
+    val i = Source.fromFile(path.toString)
+    val content = i.getLines().toString().replace("\n\r", "\n")
+
+    i.close()
+
     content
   }
 }
