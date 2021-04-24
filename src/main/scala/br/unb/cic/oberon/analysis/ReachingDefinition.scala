fe16e3ef9d78510b2865b2106d0c167ef74c1941
alexander-p30 <alexanderp30e@gmail.com>
2021-04-24 18:54:52 -0300

    fully implemented reaching definition analysis

--- src/main/scala/br/unb/cic/oberon/analysis/ReachingDefinition.scala
+++ src/main/scala/br/unb/cic/oberon/analysis/ReachingDefinition.scala
@@ -1,24 +1,12 @@
 package br.unb.cic.oberon.analysis
 
 import br.unb.cic.oberon.ast.{AssignmentStmt, ReadIntStmt}
-import br.unb.cic.oberon.cfg.{GraphNode, SimpleNode}
+import br.unb.cic.oberon.cfg.{EndNode, GraphNode, SimpleNode}
 import scalax.collection.GraphEdge
 import scalax.collection.mutable.Graph
 
 class ReachingDefinition {
   def analyseReachingDefinitions(cfg: Graph[GraphNode, GraphEdge.DiEdge]): Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = {
-    // Inicializar In e Out vazio
-    // Map[GraphNode => Set[GraphNode]]
-    // cfg.nodes.foreach(n => inicializar)
-    // cfg.edges.foreach(e => e match {
-    //    case n1 ~> n2 =>
-    //      n2 "In" = n1 "Out"
-    // })
-    // 1ª Estratégia -> get StartNode(); obter todos os edges com StartNode(); travessia no grafo (BFS ou DFS)
-    // 2ª Estratégia -> utilizar ordering http://www.scala-graph.org/guides/core-traversing.html
-    // 1. Copiar todas as saídas do nó anterior para a entrada do nó atual
-    // 2. GraphNode -> se for um AssingmentStatment, adicionamos no out uma tupla (var_name, GraphNode)
-
     var reachingDefinitions: Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = initializeReachingDefinitions(cfg)
     var fixedPointReached: Boolean = false
     var previousReachingDefinitions: Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = reachingDefinitions
@@ -30,6 +18,7 @@ class ReachingDefinition {
             val previousNode: GraphNode = previousNodeT.toOuter
             val currentNode: GraphNode = currentNodeT.toOuter
             val previousNodeOut: Set[(String, GraphNode)] = reachingDefinitions(previousNode)._2
+            val currentNodeIn: Set[(String, GraphNode)] = reachingDefinitions(currentNode)._1 | previousNodeOut
 
             val currentNodeGen: (String, GraphNode) = currentNode match {
               case SimpleNode(AssignmentStmt(varName, _)) =>
@@ -40,17 +29,18 @@ class ReachingDefinition {
                 null
             }
 
+            val currentNodeKill: Set[(String, GraphNode)] = if(currentNodeGen != null) currentNodeIn.filter(definition => definition._1 == currentNodeGen._1) else Set()
             // OUT(x) = In(x) + gen(x) - kill(x)
-            val currentNodeOut: Set[(String, GraphNode)] = if(currentNodeGen != null) {
-              currentNodeGen :: previousNodeOut.filter(definition => definition._1 != currentNodeGen._1)
-            } else previousNodeOut
+            val currentNodeOut: Set[(String, GraphNode)] = if(currentNodeGen != null) currentNodeIn ++ Set(currentNodeGen) -- currentNodeKill else currentNodeIn
 
-            reachingDefinitions = reachingDefinitions + (currentNode -> (previousNodeOut, currentNodeOut))
+            reachingDefinitions = reachingDefinitions + (currentNode -> (currentNodeIn, currentNodeOut))
         }
       )
       fixedPointReached = previousReachingDefinitions == reachingDefinitions
       previousReachingDefinitions = reachingDefinitions
     }
+
+    reachingDefinitions = reachingDefinitions + (EndNode() -> (reachingDefinitions(EndNode())._1, Set()))
 
     reachingDefinitions
   }
--- src/test/scala/br/unb/cic/oberon/analysis_test/ReachingDefinitionTest.scala
+++ src/test/scala/br/unb/cic/oberon/analysis_test/ReachingDefinitionTest.scala
@@ -33,9 +33,6 @@ class ReachingDefinitionTest extends AnyFunSuite {
 
     val numberOfMapKeys = reachingDefinitionAnalysis.keySet.size
 
-//    Map(StartNode() -> (List(),List()), SimpleNode(ReadIntStmt(x)) -> (List((max,SimpleNode(AssignmentStmt(max,VarExpression(x)))), (x,SimpleNode(ReadIntStmt(x)))),List((x,SimpleNode(ReadIntStmt(x))), (max,SimpleNode(AssignmentStmt(max,VarExpression(x)))))), SimpleNode(AssignmentStmt(max,VarExpression(x))) -> (List((x,SimpleNode(ReadIntStmt(x)))),List((max,SimpleNode(AssignmentStmt(max,VarExpression(x)))), (x,SimpleNode(ReadIntStmt(x))))), EndNode() -> (List((x,SimpleNode(ReadIntStmt(x))), (max,SimpleNode(AssignmentStmt(max,VarExpression(x))))),List((x,SimpleNode(ReadIntStmt(x))), (max,SimpleNode(AssignmentStmt(max,VarExpression(x)))))))
-//    Map(StartNode() -> (List(),List()), SimpleNode(ReadIntStmt(x)) -> (List((max,SimpleNode(AssignmentStmt(max,VarExpression(x)))), (x,SimpleNode(ReadIntStmt(x)))),List((x,SimpleNode(ReadIntStmt(x))), (max,SimpleNode(AssignmentStmt(max,VarExpression(x)))))), SimpleNode(AssignmentStmt(max,VarExpression(x))) -> (List((x,SimpleNode(ReadIntStmt(x)))),List((max,SimpleNode(AssignmentStmt(max,VarExpression(x)))), (x,SimpleNode(ReadIntStmt(x))))), EndNode() -> (List((x,SimpleNode(ReadIntStmt(x)))),List((x,SimpleNode(ReadIntStmt(x))))))
-
     assert(numberOfNodes == numberOfMapKeys)
   }
   /*
@@ -70,7 +67,20 @@ class ReachingDefinitionTest extends AnyFunSuite {
     assert(expected == reachingDefinitionAnalysis)
   }
 
-  ignore("return a map with fixed point for reaching definitions") {
+  /**
+   * This is test case simulates the following oberon code:
+   *
+   * BEGIN
+   *   readInt(x);
+   *   readInt(max);
+   *   IF(x > max) THEN
+   *     max := x
+   *   END;
+   *   write(max)
+   * END
+   *
+   */
+  test("return a map with fixed point for reaching definitions") {
     val s3_1 = AssignmentStmt("max", VarExpression("x"))
     val s1 = ReadIntStmt("x")
     val s2 = ReadIntStmt("max")
@@ -87,30 +97,18 @@ class ReachingDefinitionTest extends AnyFunSuite {
     cfg += SimpleNode(s4) ~> EndNode()
 
     val expected = Map(
-      StartNode() -> (List(), List()),
-      SimpleNode(s1) -> (List(), List(("x", s1))),
-      SimpleNode(s2) -> (List(("x", s1)), List(("x", 1), ("max", 2))),
-      SimpleNode(s3) -> (List(("x", s1)), List(("x", 1), ("max", 2))),
-      SimpleNode(s3_1) -> (List(("x", 1), ("max", 2)), List(("x", 1), ("max", 4))),
-      SimpleNode(s4) -> (List(("x", 1), ("max", 2), ("max", 4)), List(("x", 1), ("max", 2), ("max", 4))),
-      EndNode() -> (List(), List())
+      StartNode() -> (Set(), Set()),
+      SimpleNode(s1) -> (Set(), Set(("x", SimpleNode(s1)))),
+      SimpleNode(s2) -> (Set(("x", SimpleNode(s1))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)))),
+      SimpleNode(s3) -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)))),
+      SimpleNode(s3_1) -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s3_1)))),
+      SimpleNode(s4) -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)), ("max", SimpleNode(s3_1))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)), ("max", SimpleNode(s3_1)))),
+      EndNode() -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)), ("max", SimpleNode(s3_1))), Set())
     )
-    //    val expected = Map(
-    //      s1 -> Map("In" -> List(), "Out" -> List(("x", 1))),
-    //      s2 -> Map("In" -> List(("x", 1)), "Out" -> List(("x", 1), ("max", 2))),
-    //      s3 -> Map("In" -> List(("x", 1), ("max", 2)), "Out" -> List(("x", 1), ("max", 2))),
-    //      s3_1 -> Map("In" -> List(("x", 1), ("max", 2)), "Out" -> List(("x", 1), ("max", 4))),
-    //      s4 -> Map("In" -> List(("x", 1), ("max", 2), ("max", 4)), "Out" -> List(("x", 1), ("max", 2), ("max", 4)))
-    //    )
-    // Outra estrutura possível: mapeamento de tupla de listas
-    // val expected = Map(
-    //   s1 -> ( List(), List( ("x", s1) ) ),
-    //   s2 -> (List(In), List(Out))
-    // )
 
     val reachingDefinition = new ReachingDefinition()
     val reachingDefinitionAnalysis = reachingDefinition.analyseReachingDefinitions(cfg)
-    info(s"$reachingDefinitionAnalysis")
+
     assert(expected == reachingDefinitionAnalysis)
   }
 
