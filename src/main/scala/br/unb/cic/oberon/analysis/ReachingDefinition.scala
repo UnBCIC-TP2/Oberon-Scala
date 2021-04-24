d130644c83684dc280dea304360c23921cbdeb72
alexander-p30 <alexanderp30e@gmail.com>
2021-04-24 19:32:06 -0300

    use control flow graph analysis object on reaching definition

--- src/main/scala/br/unb/cic/oberon/analysis/ReachingDefinition.scala
+++ src/main/scala/br/unb/cic/oberon/analysis/ReachingDefinition.scala
@@ -1,15 +1,15 @@
 package br.unb.cic.oberon.analysis
 
-import br.unb.cic.oberon.ast.{AssignmentStmt, ReadIntStmt}
-import br.unb.cic.oberon.cfg.{EndNode, GraphNode, SimpleNode}
+import br.unb.cic.oberon.cfg.{EndNode, GraphNode}
 import scalax.collection.GraphEdge
 import scalax.collection.mutable.Graph
+import br.unb.cic.oberon.analysis.ControlFlowGraphAnalysis._
 
 class ReachingDefinition {
-  def analyseReachingDefinitions(cfg: Graph[GraphNode, GraphEdge.DiEdge]): Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = {
-    var reachingDefinitions: Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = initializeReachingDefinitions(cfg)
+  def analyseReachingDefinitions(cfg: Graph[GraphNode, GraphEdge.DiEdge]): ReachingDefinitionMapping = {
+    var reachingDefinitions: ReachingDefinitionMapping = initializeReachingDefinitions(cfg)
     var fixedPointReached: Boolean = false
-    var previousReachingDefinitions: Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = reachingDefinitions
+    var previousReachingDefinitions: ReachingDefinitionMapping = reachingDefinitions
 
     while(!fixedPointReached) {
       cfg.edges.foreach(
@@ -17,21 +17,16 @@ class ReachingDefinition {
           case GraphEdge.DiEdge(previousNodeT, currentNodeT) =>
             val previousNode: GraphNode = previousNodeT.toOuter
             val currentNode: GraphNode = currentNodeT.toOuter
-            val previousNodeOut: Set[(String, GraphNode)] = reachingDefinitions(previousNode)._2
-            val currentNodeIn: Set[(String, GraphNode)] = reachingDefinitions(currentNode)._1 | previousNodeOut
+            val currentNodeIn: NodeDefinitionSet = calculateNodeDefinitionIn(reachingDefinitions, currentNode, previousNode)
+            val currentNodeGen: NodeDefinitionSet = calculateNodeGenDefinitions(currentNode)
+            val currentNodeKill: NodeDefinitionSet = calculateNodeKillDefinitions(currentNodeIn, currentNodeGen)
 
-            val currentNodeGen: (String, GraphNode) = currentNode match {
-              case SimpleNode(AssignmentStmt(varName, _)) =>
-                (varName, currentNode)
-              case SimpleNode(ReadIntStmt(varName)) =>
-                (varName, currentNode)
-              case _ =>
-                null
-            }
-
-            val currentNodeKill: Set[(String, GraphNode)] = if(currentNodeGen != null) currentNodeIn.filter(definition => definition._1 == currentNodeGen._1) else Set()
             // OUT(x) = In(x) + gen(x) - kill(x)
-            val currentNodeOut: Set[(String, GraphNode)] = if(currentNodeGen != null) currentNodeIn ++ Set(currentNodeGen) -- currentNodeKill else currentNodeIn
+            val currentNodeOut: NodeDefinitionSet =
+              if(currentNode != EndNode())
+                currentNodeIn ++ currentNodeGen -- currentNodeKill
+              else
+                Set()
 
             reachingDefinitions = reachingDefinitions + (currentNode -> (currentNodeIn, currentNodeOut))
         }
@@ -40,13 +35,11 @@ class ReachingDefinition {
       previousReachingDefinitions = reachingDefinitions
     }
 
-    reachingDefinitions = reachingDefinitions + (EndNode() -> (reachingDefinitions(EndNode())._1, Set()))
-
     reachingDefinitions
   }
 
-  private def initializeReachingDefinitions(cfg: Graph[GraphNode, GraphEdge.DiEdge]): Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = {
-    var reachingDefinitions: Map[GraphNode, (Set[(String, GraphNode)], Set[(String, GraphNode)])] = Map()
+  private def initializeReachingDefinitions(cfg: Graph[GraphNode, GraphEdge.DiEdge]): ReachingDefinitionMapping = {
+    var reachingDefinitions: ReachingDefinitionMapping = Map()
 
     cfg.edges.foreach(
       edge => edge.nodes.foreach(
