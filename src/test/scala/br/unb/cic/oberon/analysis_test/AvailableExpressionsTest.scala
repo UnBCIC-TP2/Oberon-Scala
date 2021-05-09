f903777c78f9bc8a8433b6ed809bab4f3dbfbbc5
alexander-p30 <alexanderp30e@gmail.com>
2021-05-09 09:27:39 -0300

    test for available expressions abstraction initialization

--- src/test/scala/br/unb/cic/oberon/analysis_test/AvailableExpressionsTest.scala
+++ src/test/scala/br/unb/cic/oberon/analysis_test/AvailableExpressionsTest.scala
@@ -1,6 +1,6 @@
 package br.unb.cic.oberon.analysis_test
 
-import br.unb.cic.oberon.analysis.algorithms.ReachingDefinitions
+import br.unb.cic.oberon.analysis.algorithms.AvailableExpressions
 import br.unb.cic.oberon.ast._
 import br.unb.cic.oberon.cfg.{EndNode, GraphNode, SimpleNode, StartNode}
 import org.scalatest.funsuite.AnyFunSuite
@@ -12,50 +12,42 @@ class AvailableExpressionsTest extends AnyFunSuite {
   /**
    * BEGIN
    *   readInt(x);
-   *   readInt(max);
-   *   IF(x > max) THEN
-   *     max := x
-   *   END;
-   *   write(max)
+   *   readInt(y);
+   *   max := x + y
    * END
    */
   test("return a map with number of keys equal to number of graph nodes") {
-    val s3_1 = AssignmentStmt("max", VarExpression("x"))
     val s1 = ReadIntStmt("x")
-    val s2 = ReadIntStmt("max")
-    val s3 = IfElseStmt(GTExpression(VarExpression("x"), VarExpression("max")), s3_1 , None)
-    val s4 = WriteStmt(VarExpression("max"))
+    val s2 = ReadIntStmt("y")
+    val s3 = AssignmentStmt("max", AddExpression(VarExpression("x"), VarExpression("y")))
 
     val cfg = Graph[GraphNode, GraphEdge.DiEdge]()
     cfg += StartNode() ~> SimpleNode(s1)
     cfg += SimpleNode(s1) ~> SimpleNode(s2)
     cfg += SimpleNode(s2) ~> SimpleNode(s3)
-    cfg += SimpleNode(s3) ~> SimpleNode(s3_1)
-    cfg += SimpleNode(s3_1) ~> SimpleNode(s4)
-    cfg += SimpleNode(s3) ~> SimpleNode(s4)
-    cfg += SimpleNode(s4) ~> EndNode()
+    cfg += SimpleNode(s3) ~> EndNode()
 
-    val numberOfNodes = 7
+    val numberOfNodes = 5
 
-    val reachingDefinitions = new ReachingDefinitions()
-    val reachingDefinitionsAnalysis = reachingDefinitions.analyse(cfg)
+    val algorithm = new AvailableExpressions
+    val algorithmAnalysis = algorithm.analyse(cfg)
 
-    val numberOfMapKeys = reachingDefinitionsAnalysis.keySet.size
+    val numberOfMapKeys = algorithmAnalysis.keySet.size
 
     assert(numberOfNodes == numberOfMapKeys)
   }
 
-  /*
-  * BEGIN
-  *   readInt(x);
-  *   max := x;
-  *   readInt(max);
-  * END
-  */
+  /**
+   * BEGIN
+   *   readInt(x);
+   *   readInt(y);
+   *   max := x + y
+   * END
+   */
   test("return a map with reaching definitions for each graph node (example 1)") {
     val s1 = ReadIntStmt("x")
-    val s2 = AssignmentStmt("max", VarExpression("x"))
-    val s3 = ReadIntStmt("max")
+    val s2 = ReadIntStmt("y")
+    val s3 = AssignmentStmt("max", AddExpression(VarExpression("x"), VarExpression("y")))
 
     val cfg = Graph[GraphNode, GraphEdge.DiEdge]()
     cfg += StartNode() ~> SimpleNode(s1)
@@ -65,16 +57,16 @@ class AvailableExpressionsTest extends AnyFunSuite {
 
     val expected = Map(
       StartNode() -> (Set(), Set()),
-      SimpleNode(s1) -> (Set(), Set(("x", SimpleNode(s1)))),
-      SimpleNode(s2) -> (Set(("x", SimpleNode(s1))), Set(("max", SimpleNode(s2)), ("x", SimpleNode(s1)))),
-      SimpleNode(s3) -> (Set(("max", SimpleNode(s2)), ("x", SimpleNode(s1))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s3)))),
-      EndNode() -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s3))), Set()),
+      SimpleNode(s1) -> (Set(), Set((Set(AddExpression(VarExpression("x"), VarExpression("y"))), SimpleNode(s3)))),
+      SimpleNode(s2) -> (Set(), Set((Set(AddExpression(VarExpression("x"), VarExpression("y"))), SimpleNode(s3)))),
+      SimpleNode(s3) -> (Set(), Set((Set(AddExpression(VarExpression("x"), VarExpression("y"))), SimpleNode(s3)))),
+      EndNode() -> (Set(), Set((Set(AddExpression(VarExpression("x"), VarExpression("y"))), SimpleNode(s3)))),
     )
 
-    val reachingDefinitions = new ReachingDefinitions()
-    val reachingDefinitionsAnalysis = reachingDefinitions.analyse(cfg)
+    val availableExps = new AvailableExpressions()
+    val availableExpsAnalysis = availableExps.analyse(cfg)
 
-    assert(expected == reachingDefinitionsAnalysis)
+    assert(expected == availableExpsAnalysis)
   }
 
   /**
@@ -105,17 +97,17 @@ class AvailableExpressionsTest extends AnyFunSuite {
 
     val expected = Map(
       StartNode() -> (Set(), Set()),
-      SimpleNode(s1) -> (Set(), Set(("x", SimpleNode(s1)))),
-      SimpleNode(s2) -> (Set(("x", SimpleNode(s1))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)))),
-      SimpleNode(s3) -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)))),
-      SimpleNode(s3_1) -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s3_1)))),
-      SimpleNode(s4) -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)), ("max", SimpleNode(s3_1))), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)), ("max", SimpleNode(s3_1)))),
-      EndNode() -> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2)), ("max", SimpleNode(s3_1))), Set())
+      SimpleNode(s1) -> (Set(), Set((Set(GTExpression(VarExpression("x"), VarExpression("max"))), SimpleNode(s3)))),
+      SimpleNode(s2) -> (Set(), Set((Set(GTExpression(VarExpression("x"), VarExpression("max"))), SimpleNode(s3)))),
+      SimpleNode(s3) -> (Set(), Set((Set(GTExpression(VarExpression("x"), VarExpression("max"))), SimpleNode(s3)))),
+      SimpleNode(s3_1) -> (Set(), Set((Set(GTExpression(VarExpression("x"), VarExpression("max"))), SimpleNode(s3)))),
+      SimpleNode(s4) -> (Set(), Set((Set(GTExpression(VarExpression("x"), VarExpression("max"))), SimpleNode(s3)))),
+      EndNode() -> (Set(), Set((Set(GTExpression(VarExpression("x"), VarExpression("max"))), SimpleNode(s3))))
     )
 
-    val reachingDefinitions = new ReachingDefinitions()
-    val reachingDefinitionsAnalysis = reachingDefinitions.analyse(cfg)
+    val availableExps = new AvailableExpressions()
+    val availableExpsAnalysis = availableExps.analyse(cfg)
 
-    assert(expected == reachingDefinitionsAnalysis)
+    assert(expected == availableExpsAnalysis)
   }
 }
