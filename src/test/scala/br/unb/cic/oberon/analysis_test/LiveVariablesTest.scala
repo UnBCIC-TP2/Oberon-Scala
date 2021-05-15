89f62271b89335829a4780219590768adf976c63
artur zorron <tucazorron@gmail.com>
2021-05-15 18:57:31 -0300

    fix live variables expected hash map

--- src/test/scala/br/unb/cic/oberon/analysis_test/LiveVariablesTest.scala
+++ src/test/scala/br/unb/cic/oberon/analysis_test/LiveVariablesTest.scala
@@ -1,18 +1,18 @@
 package br.unb.cic.oberon.analysis_test
 
-import br.unb.cic.oberon.analysis.algorithms.LiveVariables
-import br.unb.cic.oberon.ast.{AssignmentStmt, GTExpression, IfElseStmt, ReadIntStmt, VarExpression, WriteStmt}
-import br.unb.cic.oberon.cfg.{EndNode, GraphNode, SimpleNode, StartNode}
-import org.scalatest.funsuite.AnyFunSuite
-import scalax.collection.GraphEdge
-import scalax.collection.GraphPredef.EdgeAssoc
+import br.unb.cic.oberon.analysis.ControlFlowGraphAnalysis
 import scalax.collection.mutable.Graph
+import scalax.collection.GraphEdge
+import br.unb.cic.oberon.cfg.{EndNode, GraphNode, SimpleNode}
+import br.unb.cic.oberon.ast.{AssignmentStmt, EAssignmentStmt, WriteStmt, IfElseStmt, IfElseIfStmt, ElseIfStmt, WhileStmt, RepeatUntilStmt, ForStmt, VarExpression, Brackets, EQExpression, NEQExpression, GTExpression, LTExpression, GTEExpression, LTEExpression, AddExpression, SubExpression, MultExpression, DivExpression, OrExpression, AndExpression}
+import scala.collection.immutable.HashMap
+import scala.collection.immutable.Set
 
 class LiveVariablesTest extends AnyFunSuit {
 
-    type SetStructure = Set[(String, GraphNode)]
-    type HashMapStructure  = HashMap[GraphNode, (SetStructure, SetStructure)]
-    type GraphStructure = Graph[GraphNode, GraphEdge.DiEdge]
+    type SetStructure 		= Set[(String, GraphNode)]
+    type HashMapStructure  	= HashMap[GraphNode, (SetStructure, SetStructure)]
+    type GraphStructure 	= Graph[GraphNode, GraphEdge.DiEdge]
 
 	//	BEGIN
 	// 		readInt(x);
@@ -34,25 +34,25 @@ class LiveVariablesTest extends AnyFunSuit {
 	graph += SimpleNode(s1)   ~> SimpleNode(s2)
 	graph += SimpleNode(s2)   ~> SimpleNode(s3)
 	graph += SimpleNode(s3)   ~> SimpleNode(s3_1)
-	graph += SimpleNode(s3_1) ~> SimpleNode(s4)
 	graph += SimpleNode(s3)   ~> SimpleNode(s4)
+	graph += SimpleNode(s3_1) ~> SimpleNode(s4)
 	graph += SimpleNode(s4)   ~> EndNode()
 
 	val live_variables = new LiveVariables()
 	val graph_received = live_variables.backwardGraph(graph)
 	val hash_map_received = live_variables.initializeHashMap(graph)
-	val live_variables_received = live_variables.analyse(graph)
+	val live_variables_received = live_variables.createLiveVariables(graph_received, hash_map_received)
 
     test("BACKWARD GRAPH") {
 
 		var graph_expected: GraphStructure
 
-		graph_expected += EndNode()		~> SimpleNode(s4)
+		graph_expected += EndNode()			~> SimpleNode(s4)
 		graph_expected += SimpleNode(s4) 	~> SimpleNode(s3_1)
 		graph_expected += SimpleNode(s4) 	~> SimpleNode(s3)
 		graph_expected += SimpleNode(s3_1)	~> SimpleNode(s3)
 		graph_expected += SimpleNode(s3) 	~> SimpleNode(s2)
-		graph_expected += SimpleNode(s4) 	~> SimpleNode(s1)
+		graph_expected += SimpleNode(s2) 	~> SimpleNode(s1)
 		graph_expected += SimpleNode(s1) 	~> StartNode()
 
 		assert(graph_expected == graph_received)
@@ -78,11 +78,11 @@ class LiveVariablesTest extends AnyFunSuit {
 		var live_variables_expected: HashMapStructure
 
 		live_variables_expected += StartNode() 		~> (Set(), Set())
-		live_variables_expected += SimpleNode(s1) 	~> (Set(), Set("x", SimpleNode(s1)))
-		live_variables_expected += SimpleNode(s2) 	~> (Set("x", SimpleNode(s1)), Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2))))
-		live_variables_expected += SimpleNode(s3) 	~> (Set(("x", SimpleNode(s1)), ("max", SimpleNode(s2))), Set(("max", SimpleNode(s2)), ("x", SimpleNode(s1)))
-		live_variables_expected += SimpleNode(s3_1) ~> (Set("x", SimpleNode(s1)), Set("max", SimpleNode(s3_1)))
-		live_variables_expected += SimpleNode(s4) 	~> (Set(("max", SimpleNode(s2)), ("max", SimpleNode(s3_1))), Set())
+		live_variables_expected += SimpleNode(s1) 	~> (Set(), Set("x")
+		live_variables_expected += SimpleNode(s2) 	~> (Set("x"), Set("max", "x"))
+		live_variables_expected += SimpleNode(s3) 	~> (Set("max", "x"), Set("max", "x"))
+		live_variables_expected += SimpleNode(s3_1) ~> (Set("x"), Set("max"))
+		live_variables_expected += SimpleNode(s4) 	~> (Set("max"), Set())
 		live_variables_expected += EndNode() 		~> (Set(), Set())
 
 		assert(live_variables_expected == live_variables_received)
