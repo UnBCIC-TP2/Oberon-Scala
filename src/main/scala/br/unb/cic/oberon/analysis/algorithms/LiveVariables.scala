a0b85634b4b03e244c2f43e54f2d8570bf04ec23
alexander-p30 <alexanderp30e@gmail.com>
2021-05-19 21:42:06 -0300

    makes live variables algotithm class heritage-free

--- src/main/scala/br/unb/cic/oberon/analysis/algorithms/LiveVariables.scala
+++ src/main/scala/br/unb/cic/oberon/analysis/algorithms/LiveVariables.scala
@@ -1,6 +1,6 @@
 package br.unb.cic.oberon.analysis.algorithms
 
-import br.unb.cic.oberon.analysis.ControlFlowGraphAnalysis
+//import br.unb.cic.oberon.analysis.ControlFlowGraphAnalysis
 import scalax.collection.mutable.Graph
 import scalax.collection.GraphEdge
 import scalax.collection.GraphPredef.EdgeAssoc
@@ -8,21 +8,21 @@ import br.unb.cic.oberon.cfg.{GraphNode, StartNode, SimpleNode, EndNode}
 import br.unb.cic.oberon.ast.{AssignmentStmt, EAssignmentStmt, ReadIntStmt, WriteStmt, IfElseStmt, IfElseIfStmt, ElseIfStmt, WhileStmt, RepeatUntilStmt, ForStmt, VarExpression, Brackets, EQExpression, NEQExpression, GTExpression, LTExpression, GTEExpression, LTEExpression, AddExpression, SubExpression, MultExpression, DivExpression, OrExpression, AndExpression}
 import br.unb.cic.oberon.ast.Expression
 //import br.unb.cic.oberon.ast.Boolean
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.Set
-import scala.annotation.tailrec
-import scala.collection.mutable
+import scala.collection.immutable.HashMap
+import scala.collection.immutable.Set
+//import scala.annotation.tailrec
+//import scala.collection.mutable
 
-case class LiveVariables() extends ControlFlowGraphAnalysis[HashMap[GraphNode, (Set[String], Set[String])], Set[String]] {
+class LiveVariables() {
 	
-	type SetStructure 		= Set[String]
-	type HashMapStructure  	= HashMap[GraphNode, (SetStructure, SetStructure)]
-	type GraphStructure 	= Graph[GraphNode, GraphEdge.DiEdge]
+	type SetStructure = Set[String]
+	type HashMapStructure = HashMap[GraphNode, (SetStructure, SetStructure)]
+	type GraphStructure = Graph[GraphNode, GraphEdge.DiEdge]
 
 	def analyse(graph: GraphStructure): HashMapStructure = {
-		val initial_hash_map	= initializeHashMap(graph)
-		val backward_graph 		= backwardGraph(graph)
-		val live_variables		= createLiveVariables(backward_graph, initial_hash_map)
+		val initial_hash_map = initializeHashMap(graph)
+		val backward_graph = backwardGraph(graph)
+		val live_variables = createLiveVariables(backward_graph, initial_hash_map)
 		live_variables
 	}
 
@@ -70,7 +70,7 @@ case class LiveVariables() extends ControlFlowGraphAnalysis[HashMap[GraphNode, (
 		node_output
 	}
 
-	def nodeInput(live_variables: HashMapStructure, target_node: GraphNode, node_output: SetStructure): SetStructure = {
+	def nodeInput(_live_variables: HashMapStructure, target_node: GraphNode, node_output: SetStructure): SetStructure = {
 		val node_input: SetStructure = use(target_node) ++ (node_output -- define(target_node))
 		node_input
 	}
@@ -84,33 +84,33 @@ case class LiveVariables() extends ControlFlowGraphAnalysis[HashMap[GraphNode, (
 	}
 
 	def use(node: GraphNode): SetStructure = node match {
-		case SimpleNode(AssignmentStmt(_, expression)) 		=> getExpressionVariables(expression)
-		case SimpleNode(EAssignmentStmt(_, expression)) 	=> getExpressionVariables(expression)
-		case SimpleNode(WriteStmt(expression)) 				=> getExpressionVariables(expression)
-		case SimpleNode(IfElseStmt(expression, _, _)) 		=> getExpressionVariables(expression)
-		case SimpleNode(IfElseIfStmt(expression, _, _, _)) 	=> getExpressionVariables(expression)
-		case SimpleNode(ElseIfStmt(expression, _)) 			=> getExpressionVariables(expression)
-		case SimpleNode(WhileStmt(expression, _)) 			=> getExpressionVariables(expression)
-		case SimpleNode(RepeatUntilStmt(expression, _)) 	=> getExpressionVariables(expression)
-		case SimpleNode(ForStmt(_, expression, _)) 			=> getExpressionVariables(expression)
+		case SimpleNode(AssignmentStmt(_, expression)) => getExpressionVariables(expression)
+		case SimpleNode(EAssignmentStmt(_, expression)) => getExpressionVariables(expression)
+		case SimpleNode(WriteStmt(expression)) => getExpressionVariables(expression)
+		case SimpleNode(IfElseStmt(expression, _, _)) => getExpressionVariables(expression)
+		case SimpleNode(IfElseIfStmt(expression, _, _, _)) => getExpressionVariables(expression)
+		case SimpleNode(ElseIfStmt(expression, _)) => getExpressionVariables(expression)
+		case SimpleNode(WhileStmt(expression, _)) => getExpressionVariables(expression)
+		case SimpleNode(RepeatUntilStmt(expression, _)) => getExpressionVariables(expression)
+		case SimpleNode(ForStmt(_, expression, _)) => getExpressionVariables(expression)
 		case _ 	=> Set()
 	}
 
 	private def getExpressionVariables(expression: Expression): SetStructure = expression match {
-		case VarExpression(variable) 		=> Set(variable)
-		case Brackets(exp) 					=> getExpressionVariables(exp)
-		case EQExpression(left, right) 		=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case NEQExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case GTExpression(left, right) 		=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case LTExpression(left, right) 		=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case GTEExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case LTEExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case AddExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case SubExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case MultExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case DivExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case OrExpression(left, right) 		=> getExpressionVariables(left) ++ getExpressionVariables(right)
-		case AndExpression(left, right) 	=> getExpressionVariables(left) ++ getExpressionVariables(right)
+		case VarExpression(variable) => Set(variable)
+		case Brackets(exp) => getExpressionVariables(exp)
+		case EQExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case NEQExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case GTExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case LTExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case GTEExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case LTEExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case AddExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case SubExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case MultExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case DivExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case OrExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
+		case AndExpression(left, right) => getExpressionVariables(left) ++ getExpressionVariables(right)
 		case _ => Set()
 	}
 
@@ -124,5 +124,5 @@ case class LiveVariables() extends ControlFlowGraphAnalysis[HashMap[GraphNode, (
 
 	def getNodeOut(hash_map: HashMapStructure, node: GraphNode): SetStructure = hash_map(node)._2
 
-	def computeNodeIn(graph: GraphStructure, hash_map: HashMapStructure, node: GraphNode): SetStructure = Set()
+	def computeNodeIn(_graph: GraphStructure, _hash_map: HashMapStructure, _node: GraphNode): SetStructure = Set()
 }
