a6357118054aa55a4d55be8e0f446f9615485ea1
Rodrigo B Almeida <rbonifacio123@gmail.com>
2021-07-16 16:53:32 -0300

    additional graph test case

--- src/test/scala/br/unb/cic/oberon/graph/SimpleGraphs.scala
+++ src/test/scala/br/unb/cic/oberon/graph/SimpleGraphs.scala
@@ -2,6 +2,7 @@ package br.unb.cic.oberon.graph
 
 import org.scalatest.funsuite.AnyFunSuite
 import scalax.collection.GraphEdge
+import scalax.collection.GraphEdge.{DiEdge, ~>}
 import scalax.collection.GraphPredef.EdgeAssoc
 import scalax.collection.mutable.Graph       // mutable because we "increase" the graph after its construction
 
@@ -17,5 +18,20 @@ class SimpleGraphs extends AnyFunSuite {
 
     assert(5 == g.nodes.size)      // assert that we have five vertices (nodes)
     assert(4 == g.edges.size)      // assert that we have four edges
+  }
+
+  test("Graph traversal with pattern matching") {
+    var g = Graph[Int, GraphEdge.DiEdge]()   // init an empty, DiEdge graph whose nodes are of type Int
+
+    g += 1 ~> 2                              // add the edge from 1 -> 2
+    g += 2 ~> 3                              // add the edge from 2 -> 3
+    g += 2 ~> 4                              // add the edge from 2 -> 4
+    g += 4 ~> 5                              // add the edge from 4 -> 5
+    
+    var sum = 0
+    g.edges.foreach(e => e.edge match {
+     case n1 ~> n2 => sum += n1.value * n2.value
+    })
+    assert(36 == sum)
   }
 }
