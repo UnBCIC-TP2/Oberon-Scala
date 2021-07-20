09da87684805b4abe7290fd3847e4046fb3c03e9
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2021-07-20 07:53:37 -0300

    refactor the main class

--- src/main/scala/br/unb/cic/oberon/Main.scala
+++ src/main/scala/br/unb/cic/oberon/Main.scala
@@ -8,21 +8,26 @@ import br.unb.cic.oberon.tc.TypeChecker
 import org.rogach.scallop._
 import org.rogach.scallop.exceptions
 
-import java.beans.Expression
 import java.nio.file.{Files, Paths}
-import java.sql.Statement
 
 
+/**
+ * This is the configuration of the CLI (command line interface),
+ * using the Scallop library. For more details visit the library
+ * website: https://github.com/scallop/scallop
+ *
+ * @param arguments The string list with the command line arguments.
+ */
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
-
   version("Oberon 0.1.1-SNAPSHOT")
   banner("""Compiler and interpreter for Oberon""".stripMargin)
-  //footer("\n")
 
-  val tyc = opt[String](name = "typeChecker", short = 't', descr = "Check if a oberon program is correctly typed", argName = "Oberon program path")
+  val tc = opt[String](name = "typeChecker", short = 't', descr = "Check if a oberon program is well typed", argName = "Oberon program path")
   val interpreter = opt[String](name = "interpreter", short = 'i', descr = "Interprets an Oberon program", argName = "Oberon program path" )
-  val compile = opt[List[String]](name = "compile", short = 'c', descr = "Compiles an Oberon program to C", argName = "Oberon program path> <C program destiny path")
+  val compile = opt[List[String]](name = "compile", short = 'c', descr = "Compiles an Oberon program to C", argName = "Oberon program path> <C program output path")
   val repl = opt[Boolean](name = "REPL", short = 'R', descr = "Run Oberon REPL")
+
+  requireOne(tc, interpreter, compile, repl)
 
   override def onError(e: Throwable): Unit = e match {
     case exceptions.Help("") => printHelp()
@@ -34,91 +39,132 @@ class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
 
 }
 
-object Main {
+/**
+ * The Main program of our implementation, using a Scala
+ * object. In this case, we opted for inheriting from the
+ * App trait.
+ *
+ * see more information here:
+ *    * https://www.scala-lang.org/documentation/your-first-lines-of-scala.html
+ */
+object Main extends App {
 
-  def main(args: Array[String]) {
-    val conf = new Conf(args)
-    try {
-      if (conf.tyc.isSupplied) {
+  /* main block of code */
+  val conf = new Conf(args)
 
-        val path = Paths.get(conf.tyc())
-        if (Files.exists(path)) {
-          val content = String.join("\n", Files.readAllLines(path))
-
-          val module = ScalaParser.parse(content)
-          val visitor = new TypeChecker()
-
-          val errors = visitor.visit(module)
-          errors.foreach(v => if (v._2 != "None") println(v))
-          if (errors.isEmpty) println("The code is correctly typed")
-          else println("Type error detected")
-        }
-        else {
-          println("The file '" + conf.tyc() + "' does not exist")
-        }
-
-      }
-
-      if (conf.interpreter.isSupplied) {
-
-        val path = Paths.get(conf.interpreter())
-
-        if (Files.exists(path)) {
-          val interpreter = new Interpreter()
-
-          val content = String.join("\n", Files.readAllLines(path))
-          val module = ScalaParser.parse(content)
-
-          val result = module.accept(interpreter)
-        }
-        else {
-          println("The file '" + conf.interpreter() + "' does not exist")
-        }
-      }
-
-      if (conf.compile.isSupplied) {
-
-        val oberonPath = Paths.get(conf.compile()(0))
-
-        if (Files.exists(oberonPath)) {
-          val oberonContent = String.join("\n", Files.readAllLines(oberonPath))
-          val module = ScalaParser.parse(oberonContent)
-
-          val codeGen = PaigesBasedGenerator()
-          val generatedCCode = codeGen.generateCode(module)
-
-          var cPath = Paths.get("")
-          if (conf.compile().length == 1) {
-            cPath = Paths.get("compiled.c") // If path not provided, default path applied
-          } else cPath = Paths.get(conf.compile()(1))
-
-          val writer = Files.newBufferedWriter(cPath)
-
-          writer.write(generatedCCode)
-          writer.flush()
-          writer.close()
-        }
-        else {
-          println("The file '" + conf.compile() + "' does not exist")
-        }
-      }
+  try {
+    if (conf.tc.isSupplied) {
+      typeCheck(conf)
     }
-    catch {
-      case t : Throwable => println(t.getMessage)
+    else if (conf.interpreter.isSupplied) {
+      interpret(conf)
     }
-
-    if(conf.repl.isSupplied) {
-      val replObject = new Repl()
-      replObject.runREPL()
+    else if (conf.compile.isSupplied) {
+      compile(conf)
     }
+    else if (conf.repl.isSupplied) {
+      runREPL()
+    }
+  }
+  catch {
+    case t: Throwable => println(t.getMessage)
+  }
+  if (conf.args.isEmpty) conf.printHelp()
 
-    if(conf.args.isEmpty) conf.printHelp()
+  /* end of the main block */
 
+  /**
+   * Executes the REPL (Read, Eval, Print, Loop) program
+   */
+  private def runREPL() = {
+    Repl.runREPL()
+  }
+
+  /**
+   * Executes the Oberon compiler.
+   *
+   * @param conf the CLI configuration
+   */
+  private def compile(conf: Conf) = {
+    val oberonPath = Paths.get(conf.compile()(0))
+
+    if (Files.exists(oberonPath)) {
+      val oberonContent = String.join("\n", Files.readAllLines(oberonPath))
+      val module = ScalaParser.parse(oberonContent)
+
+      val codeGen = PaigesBasedGenerator()
+      val generatedCCode = codeGen.generateCode(module)
+
+      var cPath = Paths.get("")
+      if (conf.compile().length == 1) {
+        cPath = Paths.get("compiled.c") // If path not provided, default path applied
+      } else cPath = Paths.get(conf.compile()(1))
+
+      val writer = Files.newBufferedWriter(cPath)
+
+      writer.write(generatedCCode)
+      writer.flush()
+      writer.close()
+    }
+    else {
+      println("The file '" + conf.compile() + "' does not exist")
+    }
+  }
+
+  /**
+   * Executes the Oberon interpreter.
+   *
+   * @param conf the CLI configuration
+   */
+  private def interpret(conf: Conf) = {
+    val path = Paths.get(conf.interpreter())
+
+    if (Files.exists(path)) {
+      val interpreter = new Interpreter()
+
+      val content = String.join("\n", Files.readAllLines(path))
+      val module = ScalaParser.parse(content)
+
+      val result = module.accept(interpreter)
+    }
+    else {
+      println("The file '" + conf.interpreter() + "' does not exist")
+    }
+  }
+
+  /**
+   * Executes the type checker
+   *
+   * @param conf the CLI configuration
+   */
+  private def typeCheck(conf: Conf) = {
+    val path = Paths.get(conf.tc())
+    if (Files.exists(path)) {
+      val content = String.join("\n", Files.readAllLines(path))
+
+      val module = ScalaParser.parse(content)
+      val visitor = new TypeChecker()
+
+      val errors = visitor.visit(module)
+      errors.foreach(v => if (v._2 != "None") println(v))
+      if (errors.isEmpty) println("The code is correctly typed")
+      else println("Type error detected")
+    }
+    else {
+      println("The file '" + conf.tc() + "' does not exist")
+    }
   }
 }
 
-class Repl {
+/**
+ * Our REPL module singleton class (i.e., an
+ * Scala object).
+ */
+object Repl {
 
+  /**
+   * Executes the REPL Oberon interpreter
+   */
   def runREPL(): Unit = {
     var keepRunning = true
     var input = ""
