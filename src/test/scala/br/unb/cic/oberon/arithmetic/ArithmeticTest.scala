6ae07bb5b48171a94145d5ac8e89989f56ccd588
TheKipmaster <fkipman@hotmail.com>
2021-09-08 21:16:12 -0300

    Implements tests for set operations at AST level

--- src/test/scala/br/unb/cic/oberon/arithmetic/ArithmeticTest.scala
+++ src/test/scala/br/unb/cic/oberon/arithmetic/ArithmeticTest.scala
@@ -1,6 +1,6 @@
 package br.unb.cic.oberon.arithmetic
 
-import br.unb.cic.oberon.ast.{IntValue, RealValue}
+import br.unb.cic.oberon.ast.{IntValue, RealValue, SetValue}
 import org.scalatest.funsuite.AnyFunSuite
 
 class ArithmeticTestSuite extends AnyFunSuite {
@@ -32,5 +32,37 @@ class ArithmeticTestSuite extends AnyFunSuite {
     expected = IntValue(4)
 
     assert(expected == (i09 / i02))
+  }
+
+  test("Test set union") {
+    val set1 = SetValue(Set(1,2,3))
+    val set2 = SetValue(Set(4,5,6))
+    val expected = SetValue(Set(1,2,3,4,5,6))
+
+    assert(expected == (set1 + set2))
+  }
+
+  test("Test set difference") {
+    val set1 = SetValue(Set(1,2,3,4,5,6))
+    val set2 = SetValue(Set(4,5,6))
+    val expected = SetValue(Set(1,2,3))
+
+    assert(expected == (set1 - set2))
+  }
+
+  test("Test symmetric set difference") {
+    val set1 = SetValue(Set(1,2,3,4,5))
+    val set2 = SetValue(Set(4,5,6,7,8))
+    val expected = SetValue(Set(1,2,3,6,7,8))
+
+    assert(expected == (set1 / set2))
+  }
+
+  test("Test set intersection") {
+    val set1 = SetValue(Set(1,2,3,4))
+    val set2 = SetValue(Set(4,5,6,7))
+    val expected = SetValue(Set(4))
+
+    assert(expected == (set1 * set2))
   }
 }
