935586b1083630a07cb8d2d6cdb8ae2bb8cd78e5
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2021-09-21 07:45:04 -0300

    support for the String type

--- src/main/scala/br/unb/cic/oberon/stdlib/StandardLibrary.scala
+++ src/main/scala/br/unb/cic/oberon/stdlib/StandardLibrary.scala
@@ -2,19 +2,11 @@ package br.unb.cic.oberon.stdlib
 
 import br.unb.cic.oberon.ast._
 import br.unb.cic.oberon.environment.Environment
-import scala.io.Source
+
 
 class StandardLibrary[T](env: Environment[T]) {
-    def readF (path:String) : String={
-      var string = ""
-      var bufferFile = Source.fromFile(path)
-      for (line <- bufferFile.getLines()){
-        string = string.concat(line)
-      }
-      bufferFile.close
-      string
-    }
-    val stdlib = OberonModule("STDLIB", Set.empty[String], List(), List(), List(), List(abs, odd, readFile), None)
+
+    val stdlib = OberonModule("STDLIB", Set.empty[String], List(), List(), List(), List(abs, odd), None)
 
     def abs = Procedure(
         "ABS",                             // name
@@ -40,20 +32,7 @@ class StandardLibrary[T](env: Environment[T]) {
         List(),
         List(),
 
-        SequenceStmt(
-            List(MetaStmt(() => ReturnStmt(BoolValue((env.lookup("x").get.asInstanceOf[IntValue].value % 2) != 0))))
-        )
-      )
+        MetaStmt(() => ReturnStmt(BoolValue((env.lookup("x").get.asInstanceOf[IntValue].value % 2) != 0)))
 
-      def readFile = Procedure(
-        "READFILE",
-        List(FormalArg("x",StringType)),
-        Some(StringType),
-        List(),
-        List(),
-
-        SequenceStmt(
-          List(MetaStmt(()=>ReturnStmt(StringValue(this.readF(env.lookup("x").get.asInstanceOf[StringValue].value)))))
-        )
-      )
+    )
 }
