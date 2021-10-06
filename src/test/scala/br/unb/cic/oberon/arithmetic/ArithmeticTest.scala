dcb3b6017d90043a5754372ddac1b5ecc77e3420
TheKipmaster <fkipman@hotmail.com>
2021-10-06 10:21:16 -0300

    Removes tests for set operations which haven't been implemented yet

--- src/test/scala/br/unb/cic/oberon/arithmetic/ArithmeticTest.scala
+++ src/test/scala/br/unb/cic/oberon/arithmetic/ArithmeticTest.scala
@@ -33,36 +33,4 @@ class ArithmeticTestSuite extends AnyFunSuite {
 
     assert(expected == (i09 / i02))
   }
-
-  test("Test set union") {
-    val set1 = SetValue(Set(1,2,3))
-    val set2 = SetValue(Set(4,5,6))
-    val expected = SetValue(Set(1,2,3,4,5,6))
-
-    assert(expected == (set1 + set2))
-  }
-
-  test("Test set difference") {
-    val set1 = SetValue(Set(1,2,3,4,5,6))
-    val set2 = SetValue(Set(4,5,6))
-    val expected = SetValue(Set(1,2,3))
-
-    assert(expected == (set1 - set2))
-  }
-
-  test("Test symmetric set difference") {
-    val set1 = SetValue(Set(1,2,3,4,5))
-    val set2 = SetValue(Set(4,5,6,7,8))
-    val expected = SetValue(Set(1,2,3,6,7,8))
-
-    assert(expected == (set1 / set2))
-  }
-
-  test("Test set intersection") {
-    val set1 = SetValue(Set(1,2,3,4))
-    val set2 = SetValue(Set(4,5,6,7))
-    val expected = SetValue(Set(4))
-
-    assert(expected == (set1 * set2))
-  }
 }
