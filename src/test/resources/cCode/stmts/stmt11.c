9f15c058a244c51951ccc00df44a5438caa51bf1
jhon <joaopedrotj@hotmail.com>
2022-02-28 20:23:30 -0300

    Lack of support for brackets in Ccode generator

--- src/test/resources/cCode/stmts/stmt11.c
+++ src/test/resources/cCode/stmts/stmt11.c
@@ -7,7 +7,7 @@ void main() {
   scanf("%d", &x);
   for (y = 0; y < x; y++) {
     scanf("%d", &z);
-    z = z / (y + 1);
+    z = z / (y + 1); // Brackets in arithmetic expressions is not supported by C code generator
     printf("%d\n", z);
   }
 }
