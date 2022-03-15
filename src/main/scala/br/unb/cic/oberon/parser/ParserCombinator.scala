fc4fac17a283e5d4ad480b8360a7edb80d243265
Marcelo <marcelojunqueiraf@gmail.com>
2022-03-15 10:50:56 -0300

    feat: identifier parser
    Regex refactor to accept negative numbers
    Created regex abstraction

--- src/main/scala/br/unb/cic/oberon/parser/ParserCombinator.scala
+++ src/main/scala/br/unb/cic/oberon/parser/ParserCombinator.scala
@@ -5,7 +5,11 @@ import br.unb.cic.oberon.ast._
 
 object Oberon2ScalaParser extends JavaTokenParsers {
     def bool: Parser[BoolValue] = "(FALSE|TRUE)".r ^^ (i => BoolValue(i=="TRUE"))
-    def int: Parser[IntValue] = "[0-9]+".r ^^ { (i => IntValue(i.toInt))   }
-    def real: Parser[RealValue] =  ("[0-9]+\\.[0-9]+".r | "[0-9]+".r) ^^ (i => RealValue(i.toDouble))
+    def int: Parser[IntValue] = ("-?" + number).r ^^ (i => IntValue(i.toInt))
+    def real: Parser[RealValue] =  (("-?" + number + "\\." + number).r | ("-?"+ number).r) ^^ (i => RealValue(i.toDouble))
     def string: Parser[StringValue] = ("\"[^\"]+\"".r | "\'[^\']+\'".r)  ^^ (i =>  StringValue(i.substring(1, i.length()-1)))
+    def digit: String = "[0-9]"
+    def number: String = digit + "+"
+    def alpha: String = "[A-z]"
+    def identifier: Parser[String] = (alpha +"(" + alpha + "|" + digit + "|_)*").r ^^ (i => i)
 }
--- src/test/scala/br/unb/cic/oberon/parser/ParserCombinatorTest.scala
+++ src/test/scala/br/unb/cic/oberon/parser/ParserCombinatorTest.scala
@@ -21,14 +21,22 @@ class ParserCombinatorTestSuite extends AnyFunSuite{
     }
 
     test("Testing Real Parser") {
-        val assign = RealValue(1.02)
+        val positiveReal = RealValue(1.02)
+        val negativeReal = RealValue(-3.14)
+
         val a = Oberon2ScalaParser.parse(Oberon2ScalaParser.real, "1.02") match {
             case Oberon2ScalaParser.Success(matched,_) => matched 
             case Oberon2ScalaParser.Failure(msg,_)  => fail("FAILURE: " + msg)
             case Oberon2ScalaParser.Error(msg,_) => fail("ERROR: " + msg)
         }
+        val b = Oberon2ScalaParser.parse(Oberon2ScalaParser.real, "-3.14") match {
+            case Oberon2ScalaParser.Success(matched,_) => matched 
+            case Oberon2ScalaParser.Failure(msg,_)  => fail("FAILURE: " + msg)
+            case Oberon2ScalaParser.Error(msg,_) => fail("ERROR: " + msg)
+        }
 
-        assert(assign == a)
+        assert(positiveReal == a)
+        assert(negativeReal == b)
     }
 
     test("Testing Bool Parser") {
@@ -71,5 +79,17 @@ class ParserCombinatorTestSuite extends AnyFunSuite{
 
         assert (a == assign)
         assert (b == assign)
+    }
+
+    test("Testing identifier parser") {
+        val assign = "teste"
+
+        val a = Oberon2ScalaParser.parse(Oberon2ScalaParser.identifier, "teste") match {
+            case Oberon2ScalaParser.Success(matched,_) => matched 
+            case Oberon2ScalaParser.Failure(msg,_)  => fail("FAILURE: " + msg)
+            case Oberon2ScalaParser.Error(msg,_) => fail("ERROR: " + msg)
+        }
+
+        assert (a == assign)
     }
 }
