279fe22da5babe470783e8b47619df30ab58661b
Marcelo <marcelojunqueiraf@gmail.com>
2022-03-22 10:01:37 -0300

    feat type parser

--- src/main/scala/br/unb/cic/oberon/parser/ParserCombinator.scala
+++ src/main/scala/br/unb/cic/oberon/parser/ParserCombinator.scala
@@ -3,7 +3,7 @@ import br.unb.cic.oberon.util.Resources
 import scala.util.parsing.combinator._
 import br.unb.cic.oberon.ast._
 
-object Oberon2ScalaParser extends JavaTokenParsers {
+trait Oberon2ScalaParser extends JavaTokenParsers {
     def bool: Parser[BoolValue] = "(FALSE|TRUE)".r ^^ (i => BoolValue(i=="TRUE"))
     def int: Parser[IntValue] = ("-?" + number).r ^^ (i => IntValue(i.toInt))
     def real: Parser[RealValue] =  (("-?" + number + "\\." + number).r | ("-?"+ number).r) ^^ (i => RealValue(i.toDouble))
@@ -13,11 +13,21 @@ object Oberon2ScalaParser extends JavaTokenParsers {
     def alpha: String = "[A-z]"
     def identifier: Parser[String] = (alpha +"(" + alpha + "|" + digit + "|_)*").r ^^ (i => i)
 
-    def parseAbs[T](result: ParseResult[T]): Any = { //ToDO: tirar esse Any
+    def typeParser: Parser[Type] = (
+        "INTEGER" ^^ (i => IntegerType)
+    |   "REAL" ^^ (i => RealType)
+    |   "CHAR" ^^ (i => CharacterType)
+    |   "BOOLEAN" ^^ (i => BooleanType)
+    |   "STRING" ^^ (i => StringType)
+    |   "NIL" ^^ (i => NullType)
+    |   identifier ^^ (i =>  ReferenceToUserDefinedType(i))
+    )
+
+    def parseAbs[T](result: ParseResult[T]): T = {
         return result match {
             case Success(matched,_) => matched
-            case Failure(msg,_)  => "FAILURE: " + msg
-            case Error(msg,_) => "ERROR: " + msg
+            case Failure(msg,_)  => throw new Exception(msg)
+            case Error(msg,_) => throw new Exception(msg)
         }
     }
 }
