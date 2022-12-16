5d8484faf11399a81640985c5a9f41cf295fa47d
hrrocha0 <211036061@aluno.unb.br>
2022-12-15 21:35:28 -0300

    Included syntax and code generator (not implemented) for Jimple

---
+++ src/main/scala/br/unb/cic/oberon/codegen/JimpleCodeGenerator.scala
@@ -0,0 +1,6 @@
+package br.unb.cic.oberon.codegen
+import br.unb.cic.oberon.ast.OberonModule
+
+object JimpleCodeGenerator extends CodeGenerator {
+    override def generateCode(module: OberonModule): String = ???
+}
