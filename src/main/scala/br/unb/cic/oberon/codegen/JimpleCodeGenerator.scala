e55838399facf951b32f047035aa9ab483618b94
hrrocha0 <99548553+hrrocha0@users.noreply.github.com>
2022-12-22 19:09:37 -0300

    Generate empty Jimple class from Oberon module.

--- src/main/scala/br/unb/cic/oberon/codegen/JimpleCodeGenerator.scala
+++ src/main/scala/br/unb/cic/oberon/codegen/JimpleCodeGenerator.scala
@@ -1,6 +1,20 @@
 package br.unb.cic.oberon.codegen
+
 import br.unb.cic.oberon.ast.OberonModule
 
 object JimpleCodeGenerator extends CodeGenerator {
-    override def generateCode(module: OberonModule): String = ???
+    val javaObject = TObject("java.lang.Object")
+
+    override def generateCode(module: OberonModule): String = {
+        val classDeclaration = ClassDecl(
+            modifiers = List(PublicModifer),
+            classType = TObject(module.name),
+            superClass = javaObject,
+            interfaces = List.empty[Type],
+            fields = List.empty[Field],
+            methods = List.empty[Method]
+        )
+
+        ???
+    }
 }
