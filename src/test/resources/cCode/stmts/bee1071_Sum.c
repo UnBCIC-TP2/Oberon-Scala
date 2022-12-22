710ab4875d9414c99aa9f3870f6cf60b2eacb1b2
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-12-22 07:13:06 -0300

    some valid c code for test cases, PokemonsBattle.c does not compile

---
+++ src/test/resources/cCode/stmts/bee1071_Sum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int input1, input2, ans, x, y, aux;
+
+
+int main() {
+    input1 = 15;
+    input2 = 12;
+    ans = 0;
+    x = input1;
+    y = input2 + 1;
+    if (input2 > input1) {
+        x = input2;
+        y = input1;
+    }
+    while (true) {
+        x = x - 1;
+        aux = x - x / 2 * 2;
+        if (aux == 1) {
+            ans = ans + x;
+        }
+        if (x == y) {
+            break; 
+        }
+    }
+}
---
+++ src/test/resources/cCode/stmts/bee2779_stmt01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int answer, n, m;
+int a[3];
+int cnt[11];
+
+
+int main() {
+    a[0] = 5;
+    a[1] = 8;
+    a[2] = 3;
+    cnt[0] = 0;
+    cnt[1] = 0;
+    cnt[2] = 0;
+    cnt[3] = 0;
+    cnt[4] = 0;
+    cnt[5] = 0;
+    cnt[6] = 0;
+    cnt[7] = 0;
+    cnt[8] = 0;
+    cnt[9] = 0;
+    n = 10;
+    m = 3;
+    answer = n;
+}
