e0ff096acad6edd5dc44f16ed58f4b6e74701faa
anasofia0 <anasofialaranja@gmail.com>
2023-01-17 23:29:37 -0300

    adiciona dois testes complexos para o gerador de expressões TAC

--- src/test/scala/br/unb/cic/oberon/codegen/TACodeGenTest.scala
+++ src/test/scala/br/unb/cic/oberon/codegen/TACodeGenTest.scala
@@ -63,4 +63,53 @@ class TACodeTest extends AnyFunSuite {
 
     assert(list == ops)
   }
+
+  test("Testing complex operation 1") {
+
+    Temporary.reset
+    val expr = MultExpression(IntValue(2), SubExpression(IntValue(1), AddExpression(IntValue(2), IntValue(3))))
+    val (t, list) = TACodeGenerator.generateExpression(expr, List())
+    // 2 * (1 - (2 + 3))
+
+    Temporary.reset
+    val t0 = new Temporary(IntegerType, 0, true)
+    val t1 = new Temporary(IntegerType, 1, true)
+    val t2 = new Temporary(IntegerType, 2, true)
+    val ops = List(
+      AddOp(Constant("2", IntegerType), Constant("3", IntegerType), t0, ""),
+      SubOp(Constant("1", IntegerType), t0, t1, ""),
+      MulOp(Constant("2", IntegerType), t1, t2, "")
+    )
+
+    // t0 = 2 + 3
+    // t1 = 1 - t0
+    // t2 = 2 * t1
+
+    assert(list == ops)
+  }
+
+  test("Testing complex operation 2") {
+
+    Temporary.reset
+    val expr = SubExpression(DivExpression(MultExpression(IntValue(2), IntValue(2)), IntValue(3)), IntValue(6))
+    val (t, list) = TACodeGenerator.generateExpression(expr, List())
+    // ((2 * 2) / 3) - 6
+
+    Temporary.reset
+    val t0 = new Temporary(IntegerType, 0, true)
+    val t1 = new Temporary(IntegerType, 1, true)
+    val t2 = new Temporary(IntegerType, 2, true)
+    val ops = List(
+      MulOp(Constant("2", IntegerType), Constant("2", IntegerType), t0, ""),
+      DivOp(t0, Constant("3", IntegerType), t1, ""),
+      SubOp(t1, Constant("6", IntegerType), t2, "")
+    )
+    // t0 = 2 * 2
+    // t1 = t0 / 3
+    // t2 = t1 - 6
+
+    assert(list == ops)
+
+
+  }
 }
