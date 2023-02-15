678ef9457d1efc95723815e95e662ad789deb5e6
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-02-15 04:28:42 -0300

    generated c code from bee1060_Positive.oberon

---
+++ src/test/resources/cCode/stmts/bee1060_Positive.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int i;
+float ans, v;
+float a[6];
+
+
+int main() {
+    i = 0;
+    ans = 0;
+    a[0] = 7;
+    a[1] = -5;
+    a[2] = 6;
+    a[3] = -3.4000000953674316;
+    a[4] = 4.599999904632568;
+    a[5] = 12;
+    while (true) {
+        if (a[i] >= 0) {
+            ans = ans + 1;
+        }
+        i = i + 1;
+        if (i == 6) {
+            break; 
+        }
+    }
+}
