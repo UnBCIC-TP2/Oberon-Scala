b6f55c879cadf9023d44a8be104b59285b949231
Eduardo Marciano <e.marciano.meneses@gmail.com>
2023-06-28 10:37:30 -0300

    Revert ".."
    This reverts commit f05165f2c6635337a7db999c5a95b2fb17ec7dce.

--- src/test/scala/br/unb/cic/oberon/interpreter/InterpreterTest.scala
+++ src/test/scala/br/unb/cic/oberon/interpreter/InterpreterTest.scala
@@ -669,68 +669,29 @@ class InterpreterTest extends AnyFunSuite with Oberon2ScalaParser {
 
   }
 
-test("Testing Test parser0") {
-  val module = parseResource("procedures/procedureTest0.oberon")
+  test("Testing Test parser1") {
+    val module = parseResource("procedures/procedureTest01.oberon")
 
-  val coreVisitor = new CoreVisitor()
-  val coreModule = coreVisitor.transformModule(module)
+    val coreVisitor = new CoreVisitor()
+    val coreModule = coreVisitor.transformModule(module)
 
-  assert(coreModule.accept(interpreter) == ())
+    coreModule.accept(interpreter)
 
-}
-test("Testing Test parser1") {
-  val module = parseResource("procedures/procedureTest01.oberon")
+  }
 
-  val coreVisitor = new CoreVisitor()
-  val coreModule = coreVisitor.transformModule(module)
 
-  coreModule.accept(interpreter)
+  test("Testing Test parser2") {
+    val module = parseResource("procedures/procedureTest02.oberon")
 
-}
-test("Testing Test parser2") {
-  val module = parseResource("procedures/procedureTest02.oberon")
+    val coreVisitor = new CoreVisitor()
+    val coreModule = coreVisitor.transformModule(module)
 
-  val coreVisitor = new CoreVisitor()
-  val coreModule = coreVisitor.transformModule(module)
+    assert(coreModule.accept(interpreter) == ())
 
-  assert(coreModule.accept(interpreter) == ())
+  }
 
-}
-test("Testing Test parser3") {
-  val module = parseResource("procedures/procedureTest03.oberon")
 
-  val coreVisitor = new CoreVisitor()
-  val coreModule = coreVisitor.transformModule(module)
-  assert(coreModule.accept(interpreter) == ())
 
-}
-test("Testing Test parser4") {
-  val module = parseResource("procedures/procedureTest04.oberon")
-
-  val coreVisitor = new CoreVisitor()
-  val coreModule = coreVisitor.transformModule(module)
-
-  assert(coreModule.accept(interpreter) == ())
-
-}
-test("Testing Test parser5") {
-  val module = parseResource("procedures/procedureTest05.oberon")
-
-  val coreVisitor = new CoreVisitor()
-  val coreModule = coreVisitor.transformModule(module)
-
-  assert(coreModule.accept(interpreter) == ())
-
-}
-test("Testing Test parser6") {
-  val module = parseResource("procedures/procedureTest06.oberon")
-
-  val coreVisitor = new CoreVisitor()
-  val coreModule = coreVisitor.transformModule(module)
-
-  assert(coreModule.accept(interpreter) == ())
-
-}
 
   test("Testing Ignore parser") {
     assert(Test("IGNORE", "firstTest", StringValue("The first test suite"), List[Constant](), List[VariableDeclaration](), AssertTrueStmt(EQExpression(VarExpression("x"), IntValue(20))))
