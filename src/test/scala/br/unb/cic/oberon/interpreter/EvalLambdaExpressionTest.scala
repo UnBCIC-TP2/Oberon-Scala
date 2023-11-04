8d90c7a420f6264ba08647c5e8fca5400385f637
jp4ntsnow <jpantonow@gmail.com>
2023-11-04 17:43:43 -0300

    tentando fazer o test

--- src/test/scala/br/unb/cic/oberon/interpreter/EvalLambdaExpressionTest.scala
+++ src/test/scala/br/unb/cic/oberon/interpreter/EvalLambdaExpressionTest.scala
@@ -9,6 +9,7 @@ class EvalLambdaExpressionTest extends AnyFunSuite {
   test("Test eval on simple values") {
     val interpreter = new Interpreter()
     val env = new Environment[Expression]()
-    val (env1,exp1) = evalExpression(env,LambdaExpression())
+    val args: List[FormalArg] = (("x",IntegerType))
+    val (env1,exp1) = evalExpression(env,LambdaExpression(args,AddExpression(IntValue(10),args)))
   }
 }
