16df53787314724dcc7b35340f12774718999a06
jpantonov <jpantonow@gmail.com>
2023-11-12 10:32:08 -0300

    funcionou o segundo teste

--- src/test/scala/br/unb/cic/oberon/interpreter/EvalLambdaExpressionTest.scala
+++ src/test/scala/br/unb/cic/oberon/interpreter/EvalLambdaExpressionTest.scala
@@ -14,24 +14,27 @@ class EvalLambdaExpressionTest extends AnyFunSuite {
     var env = new Environment[Expression]()
     var args: List[FormalArg] = List(ParameterByValue("x",IntegerType), ParameterByValue("y",IntegerType)) //(x:Integer)
     val operation = AddExpression(VarExpression("x"),VarExpression("y"))
-    val lambdaexp = LambdaExpression(args,operation) //x+10
+    val lambdaexp = LambdaExpression(args,operation) 
     val listexp = List(IntValue(5),IntValue(6))
     val exp = LambdaApplication(lambdaexp,listexp)
     val (env1,exp1) = interpreter.evalExpression(env,exp)
     assert(exp1 == IntValue(11))
   }
+  
 //a : (x: int, y: int) => x + y
   test("Test eval on arithmetic expressions (sub and div)") {
   }
 
   test("Test eval on boolean expressions ('and' and 'or')") {
-    // val interpreter = new Interpreter()
-    // var env = new Environment[Expression]()
-    // var args: List[FormalArg] = List(ParameterByValue("x",BooleanType), ParameterByValue("y",BooleanType)) //(x:Integer)
-    // val operation = OrExpression(VarExpression("x"),VarExpression("y"))
-    // val exp = LambdaExpression(args,operation) //x+10
-    // val (env1,exp1) = interpreter.evalExpression(env,exp)
-    // assert(exp1 == BoolValue(true))
+    val interpreter = new Interpreter()
+    var env = new Environment[Expression]()
+    var args: List[FormalArg] = List(ParameterByValue("x",BooleanType), ParameterByValue("y",BooleanType)) //(x:Integer)
+    val operation = OrExpression(VarExpression("x"),VarExpression("y"))
+    val lambdaexp = LambdaExpression(args,operation) 
+    val listexp = List(BoolValue(true),BoolValue(false))
+    val exp = LambdaApplication(lambdaexp,listexp)
+    val (env1,exp1) = interpreter.evalExpression(env,exp)
+    assert(exp1 == BoolValue(true))
   }
 
 
