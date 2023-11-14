a793329d903b8b42b698e1bd52a795f53cf90447
jpantonov <jpantonow@gmail.com>
2023-11-13 23:04:15 -0300

    parser sem sucesso ainda

--- src/main/scala/br/unb/cic/oberon/parser/ParserCombinator.scala
+++ src/main/scala/br/unb/cic/oberon/parser/ParserCombinator.scala
@@ -67,13 +67,13 @@ trait ExpressionParser extends BasicParsers {
     }
   // def lambdaExpressionParser: Parser[Value] =
   //   qualifiedName ~ ("(" ~> opt(argumentsParser) <~ ")") ^^ {
-  //     case name ~ None          => LambdaApplication(LambdaExpression(formalArgs,argumentsParser), argumentsParser)
-  //     case name ~ Some(argList) => LambdaApplication(LambdaExpression(formalArgs,argumentsParser), argumentsParser)
+  //     case name ~ None          => LambdaExpression(args,argumentsParser)
+  //     case name ~ Some(argList) => LambdaExpression(args,argumentsParser)
   //   }
   // def lambdaApplicationParser: Parser[Expression] = 
   //   qualifiedName ~ ("(" ~> opt(argumentsParser) <~ ")") ^^ {
-  //     case name ~ None          => LambdaApplication(LambdaExpression(formalArgs,argumentsParser), argumentsParser)
-  //     case name ~ Some(argList) => LambdaApplication(LambdaExpression(formalArgs,argumentsParser), argumentsParser)
+  //     case name ~ None          => LambdaApplication(LambdaExpression(args,argumentsParser), argumentsParser)
+  //     case name ~ Some(argList) => LambdaApplication(LambdaExpression(args,argumentsParser), argumentsParser)
   //   }
   def expValueParser: Parser[Expression] =
     real | int | char | string | bool | "NIL" ^^ (_ => NullValue)
