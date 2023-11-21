41f88fe15be36fd6a58447b29804fa41cd2386ae
uventura <ualivent@gmail.com>
2023-11-20 22:46:49 -0300

    Enabling Module Loader Tests

--- src/main/scala/br/unb/cic/oberon/parser/ModuleLoader.scala
+++ src/main/scala/br/unb/cic/oberon/parser/ModuleLoader.scala
@@ -88,8 +88,7 @@ sealed class ModuleMerger(val loader: ModuleLoader) {
     val constants = subtree.flatMap(_.constants)
     val variables = subtree.flatMap(_.variables)
     val procedures = subtree.flatMap(_.procedures)
-    val tests = subtree.flatMap(_.tests)
-    val stmt = concatStmts(subtree.flatMap(_.stmt))
+    val stmt = concatStmts(List(module).flatMap(_.stmt))
 
     OberonModule(
       modname,
