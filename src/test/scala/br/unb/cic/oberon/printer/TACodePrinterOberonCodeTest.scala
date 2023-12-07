6f1b1b4281c33db780131e49aeaf5a450ea60bf2
Ricardo Moreira <silvaricardo1997@gmail.com>
2023-12-06 22:23:43 -0300

    Refatorei e aprimorei os testes na classe TACodePrinterOberonCodeTest.
    - Adicionei comentários explicativos para cada teste.
    - Utilizei nomes mais descritivos para os testes.
    - Documentei a saída TAC esperada em cada teste.
    - Removi imports não utilizados para manter o código limpo.
    - Ajustei a formatação para melhorar a legibilidade.

--- src/test/scala/br/unb/cic/oberon/printer/TACodePrinterOberonCodeTest.scala
+++ src/test/scala/br/unb/cic/oberon/printer/TACodePrinterOberonCodeTest.scala
@@ -16,37 +16,41 @@ class TACodePrinterOberonCodeTest extends AnyFunSuite {
   val t4 = new Temporary(BooleanType, 4, true)
 
   test("Print 'Add' expression between two constants") {
-
+    // Given
     val expr = AddExpression(IntValue(1), IntValue(2))
     val (t, operationList) = TACodeGenerator.generateExpression(expr, List())
 
+    // Expected
     val expectedOutput = bl + "t0 = 1 + 2"
-    val tacDocumentToPrint = TACodePrinter.getTacDocumentStringFormatted(operationList)
 
+    // Then
+    val tacDocumentToPrint = TACodePrinter.getTacDocumentStringFormatted(operationList)
     assertResult(expectedOutput)(tacDocumentToPrint)
   }
 
-  test("Print 'LT' expression") {
-
-    // (1 < 2)
+  test("Print 'LT' expression with positive operands") {
+    // Given
     val expr = LTExpression(IntValue(1), IntValue(2))
     val (t, list) = TACodeGenerator.generateExpression(expr, List())
 
+    // Expected
     val expectedOutput = bl + "t0 = SLT 1 2"
-    val tacDocumentToPrint = TACodePrinter.getTacDocumentStringFormatted(list)
 
+    // Then
+    val tacDocumentToPrint = TACodePrinter.getTacDocumentStringFormatted(list)
     assertResult(expectedOutput)(tacDocumentToPrint)
   }
 
-  test("Print 'GT' expression") {
-
-    // (1 > 2)
+  test("Print 'GT' expression with positive operands") {
+    // Given
     val expr = GTExpression(IntValue(1), IntValue(2))
     val (t, list) = TACodeGenerator.generateExpression(expr, List())
 
+    // Expected
     val expectedOutput = bl + "t0 = SLT 2 1"
-    val tacDocumentToPrint = TACodePrinter.getTacDocumentStringFormatted(list)
 
+    // Then
+    val tacDocumentToPrint = TACodePrinter.getTacDocumentStringFormatted(list)
     assertResult(expectedOutput)(tacDocumentToPrint)
   }
 
