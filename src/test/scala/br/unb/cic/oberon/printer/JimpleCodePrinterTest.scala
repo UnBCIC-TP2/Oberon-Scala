3c0dd99a6f47a866b3f7f597b9c097ce98b3d6f9
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2023-12-20 18:08:10 -0300

    Add test cases for all reference JIMPLE files

--- src/test/scala/br/unb/cic/oberon/printer/JimpleCodePrinterTest.scala
+++ src/test/scala/br/unb/cic/oberon/printer/JimpleCodePrinterTest.scala
@@ -32,19 +32,67 @@ class JimpleCodePrinterTest extends AnyFunSuite with Oberon2ScalaParser {
         val charsInCodeNotInTarget = jimpleCode diff jimpleTarget
         val charsInTargetNotInCode = jimpleTarget diff jimpleCode
         assert((charsInTargetNotInCode.length == 0 && charsInCodeNotInTarget.length == 0) ||
-            (stringIsWhiteSpace(charsInTargetNotInCode) && stringIsWhiteSpace(charsInCodeNotInTarget)),
-            s"\nCharacters not found in generated code:\n${charsInTargetNotInCode}\n") 
+            (stringIsWhiteSpace(charsInTargetNotInCode) && stringIsWhiteSpace(charsInCodeNotInTarget)))
+    }
+
+    def assertGeneratedJimpleMatchesReference(oberonModulePath: String, referenceJimpleFilePath: String) {
+        val (jimpleTarget, jimpleCode) = getTargetAndPrintedJimple(
+            oberonModulePath,
+            referenceJimpleFilePath,
+        )
+        compareTargetAndPrintedJimple(jimpleTarget, jimpleCode)       
     }
 
     def stringIsWhiteSpace(str: String): Boolean = {
         str.forall(_.isWhitespace)
     }
 
-    test("Testing if simple04.jimple is equal to generated JIMPLE") {
-        val (jimpleTarget, jimpleCode) = getTargetAndPrintedJimple(
+    test("Testing if generated JIMPLE from simple01.oberon matches simple01.jimple") {
+        assertGeneratedJimpleMatchesReference(
+            "simple/simple01.oberon",
+            "src/test/resources/jimpleCodes/simple01.jimple"
+        )
+    }
+
+    test("Testing if generated JIMPLE from simple02.oberon matches simple02.jimple") {
+        assertGeneratedJimpleMatchesReference(
+            "simple/simple02.oberon",
+            "src/test/resources/jimpleCodes/simple02.jimple"
+        )
+    }
+
+    test("Testing if generated JIMPLE from simple03.oberon matches simple03.jimple") {
+        assertGeneratedJimpleMatchesReference(
+            "simple/simple03.oberon",
+            "src/test/resources/jimpleCodes/simple03.jimple"
+        )
+    }
+
+    test("Testing if generated JIMPLE from simple04.oberon matches simple04.jimple") {
+        assertGeneratedJimpleMatchesReference(
             "simple/simple04.oberon",
             "src/test/resources/jimpleCodes/simple04.jimple"
         )
-        compareTargetAndPrintedJimple(jimpleTarget, jimpleCode)
+    }
+
+    test("Testing if generated JIMPLE from simple08.oberon matches simple08.jimple") {
+        assertGeneratedJimpleMatchesReference(
+            "simple/simple08.oberon",
+            "src/test/resources/jimpleCodes/simple08.jimple"
+        )
+    }
+
+    test("Testing if generated JIMPLE from simple10.oberon matches simple10.jimple") {
+        assertGeneratedJimpleMatchesReference(
+            "simple/simple10.oberon",
+            "src/test/resources/jimpleCodes/simple10.jimple"
+        )
+    }
+
+    test("Testing if generated JIMPLE from simple11.oberon matches simple11.jimple") {
+        assertGeneratedJimpleMatchesReference(
+            "simple/simple11.oberon",
+            "src/test/resources/jimpleCodes/simple11.jimple"
+        )
     }
 }
